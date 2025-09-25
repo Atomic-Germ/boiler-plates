c5393a6554a0c22e1362cd28491bf980cdaa8ce0
Casey Parker <02-germane-atomic@icloud.com>
2025-09-24 17:47:51 -0700

    Add Haskell and Lua boilerplates

---
+++ elixir/lib/elixir_boilerplate.ex
@@ -0,0 +1,18 @@
+defmodule ElixirBoilerplate do
+  @moduledoc """
+  Documentation for `ElixirBoilerplate`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirBoilerplate.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
