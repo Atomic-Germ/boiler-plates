ba42188fd116216231dfe436ae1a07a10a782079
Casey Parker <02-germane-atomic@icloud.com>
2025-09-24 17:41:37 -0700

    Add Swift, Perl, and Scala boilerplates

---
+++ scala/build.sbt
@@ -0,0 +1,5 @@
+name := "ScalaBoilerplate"
+
+version := "0.1.0"
+
+scalaVersion := "3.3.0"
---
+++ swift/Tests/SwiftBoilerplateTests/SwiftBoilerplateTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import SwiftBoilerplate
+
+final class SwiftBoilerplateTests: XCTestCase {
+    func testExample() {
+        XCTAssertEqual("Hello, Swift Boilerplate!", "Hello, Swift Boilerplate!")
+    }
+}
